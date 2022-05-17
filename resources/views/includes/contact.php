344f941d904a8158bba155f469fdfce2d5257157
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-17 02:41:12 +0200

    Created contact.php page

---
+++ resources/views/includes/contact.php
@@ -0,0 +1,23 @@
+<div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="option">Optie</label>
+    <select id="option" name="option">
+      <option value="klacht">Klacht</option>
+      <option value="vraag">Vraag</option>
+      <option value="opmerking">Opmerking</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
